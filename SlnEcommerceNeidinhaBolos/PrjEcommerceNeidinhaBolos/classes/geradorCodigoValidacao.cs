a9a233ef42066629bc8ab0fbfec1622c3a5b6f39
pesantanafraga <pesantana07@gmail.com>
2023-01-13 09:25:06 -0300

    criando a pasta das classes, adicionando os arquivos de gerar código para validação e a classe de enviar e-mail com o código

---
+++ SlnEcommerceNeidinhaBolos/PrjEcommerceNeidinhaBolos/classes/geradorCodigoValidacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrjEcommerceNeidinhaBolos.classes
+{
+    public class geradorCodigoValidacao
+    {
+        int codigoGerado;
+
+        Random codigo = new Random();
+
+    }
+}
