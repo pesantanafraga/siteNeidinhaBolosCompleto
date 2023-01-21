9ccf99c22b45f886f3969ba2fe6ffb555e2577ad
pesantanafraga <pesantana07@gmail.com>
2023-01-20 22:14:35 -0300

    Adicionando o objeto gerador de código para a validação e o objeto envia email, faltando realizar uma correção com variável e cookie

--- SlnEcommerceNeidinhaBolos/PrjEcommerceNeidinhaBolos/pages/cadastro.aspx.cs
+++ SlnEcommerceNeidinhaBolos/PrjEcommerceNeidinhaBolos/pages/cadastro.aspx.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Net.Configuration;
+using System.Net;
 
 namespace PrjEcommerceNeidinhaBolos.pages
 {
@@ -41,13 +45,13 @@ namespace PrjEcommerceNeidinhaBolos.pages
         protected void btnCadastrar_Click(object sender, EventArgs e)
         {
             //Criando o Gerador de código para validar os e-mails na hora do cadastro
-            int codigo;//criando a variável que receberá o valor do código gerado
+            string codigo;//criando a variável que receberá o valor do código gerado
             try
             {
                 Random newCode = new Random();//criando o objeto Random que irá criar o número inteiro aleatório
                 for (int i = 0; i < 10; i++)
                 {
-                    codigo = newCode.Next(1000000);//Aplicando o valor do código gerado para a variável
+                    codigo = newCode.Next(1000000).ToString();//Aplicando o valor do código gerado para a variável
                 }
             }
             catch (Exception cod)
@@ -55,9 +59,50 @@ namespace PrjEcommerceNeidinhaBolos.pages
                 lblAlerta.Text = cod.Message;
             }
 
-            //Criando o cookie que irá guardar o valor do código gerado e validar o mesmo na tela de validação de e-mail
+            /*-------------------- Criando o cookie que vai guardar o código para validar --------------------*/
 
-            HttpCookie guardaCod = new HttpCookie();
+            /*
+
+            HttpCookie cookieCodigo = new HttpCookie("codigo");//criando o cookie que irá receber o valor do código
+            cookieCodigo.Value = codigo.ToString();
+            DateTime agora = new DateTime.Now;//Criando o objeto que irá guardar a hora atual da criação do cookie
+            TimeSpan tempo = new TimeSpan(0, 10, 0);//Criando tempo de expiração para o cookie
+            cookieCodigo.Expires = agora + tempo;//Definindo tempo de expiração para o cookie
+
+            Response.Cookies.Add(cookieCodigo);//Adicionando o cookie ao navegador
+
+            */
+
+
+            /*-------------------- Criando o objeto EnviaEmail --------------------*/
+
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();//criando o objeto
+            client.Host = "smtp.gmail.com";//definindo o servidor smtp do e-mail que enviará os e-mails
+            client.EnableSsl = true;//falando que o ssl do host estará habilitada
+            client.Credentials = new System.Net.NetworkCredential("pedrosantanaprojetos@gmail.com", "Pedro@123");//passando as credenciais do e-mail que enviará as mensagens
+            MailMessage mail = new MailMessage();//Criando um email para enviar
+            mail.Sender = new System.Net.Mail.MailAddress("pedrosantanaprojetos@gmail.com", "Pedro Santana");//passando o e-mail que enviará a menssagem e o nome do mesmo
+            mail.From = new MailAddress("pedrosantanaprojetos@gmail.com", "Pedro Santana");//De quem vai ser enviado
+            mail.To.Add(new MailAddress(txtEmailUser.Text, txtNomeUser.Text));//pegando as informações de quem vai receber
+            mail.Subject = "Código para validação de e-mail";//passando qual será o assunto do e-mail
+            mail.Body = "Parabéns, sua conta no nosso site foi criada com sucesso! <br/> Para você poder acessar a plataforma e desfrutar das nossas ofertas será necessário validar seu e-mail.<br/> <br/> Seu código de ativação é: <br/>" + codigo + "<br/> O código é valido por 10 minutos até ser usado, caso não consiga validar dentre este tempo será necessário enviar outro!"; //passando a mensagem no corpo do e-mail
+            mail.IsBodyHtml = true;//falando que o corpo do email é em html
+            mail.Priority = MailPriority.High;//passando a prioridade do e-mail
+            try
+            {
+                client.Send(mail);//fazendo o objeto enviar o email
+            }
+            catch (System.Exception erro)
+            {
+                lblAlerta.Text = erro.Message;
+            }
+            finally
+            {
+                mail = null;//passando que ao final do procedimento o mail será tudo nulo novamente
+            }
+
+            /*-------------------- Conexão com banco para executar as procedures --------------------*/
+
         }
     }
 }
